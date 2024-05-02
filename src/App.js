da9df7bd0de0b0415dd54397d2266288c401cf76
Mayank2112 <30808mayank21@gmail.com>
2024-05-02 14:10:39 +0530

    Protected routes and components setup

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import Home from './pages/Home';
+import LeftBar from './components/leftBar/LeftBar';
+import Navbar from './components/navbar/Navbar';
+import Rightbar from './components/rightBar/Rightbar';
+import Home from './pages/home/Home';
+import Profile from './pages/profile/Profile';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, Navigate, Outlet, RouterProvider } from 'react-router-dom';
 function App() {
-  
+  const currentUser = false;
+
+  const Layout = ()=>{
+    return (
+      <div>
+        <Navbar/>
+        <div style={{display:'flex'}}>
+          <LeftBar/>
+          <Outlet/>
+          <Rightbar/>
+        </div>
+      </div>
+    )
+  }
+  const ProtectedRoute = ({children})=>{
+      if(!currentUser){
+        return <Navigate to='/login'/>
+      }
+
+      return children
+  }
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: 
+        <ProtectedRoute>
+          <Layout/>
+        </ProtectedRoute>,
+      children:[
+        {
+          path:'/',
+          element: <Home/>
+        },
+        {
+          path:'/profile/:id',
+          element:<Profile/>
+        }
+      ]
+    },
+    {
+      path:'/login',
+      element: <Login/>
+    },
+    {
+      path:'/register',
+      element: <Register/>
+    }
+  ])
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router}/>
     </>
   );
 }
